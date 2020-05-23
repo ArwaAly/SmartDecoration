09da859ddca25e1c1de9b01b9433d05468a6f105
samamohamed97 <samsemo693@gmail.com>
2020-05-23 08:24:29 +0300

    profile,signup,signin,firstpage

---
+++ app/src/main/java/com/example/smartdecoration/first_page.java
@@ -0,0 +1,15 @@
+package com.example.smartdecoration;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class first_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_page);
+    }
+}
+
---
+++ app/src/main/java/com/example/smartdecoration/sign_up.java
@@ -0,0 +1,15 @@
+package com.example.smartdecoration;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
+
