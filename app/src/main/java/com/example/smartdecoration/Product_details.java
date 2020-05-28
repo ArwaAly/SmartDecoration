dc124195e5f400e280d9847dabb36d73e083c6ba
PassantHassan <passant97@gmail.com>
2020-05-28 14:27:14 +0200

    product_details,shopping_cart,shopping_cart_items,toolbar

---
+++ app/src/main/java/com/example/smartdecoration/Product_details.java
@@ -0,0 +1,15 @@
+package com.example.smartdecoration;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Product_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_details);
+    }
+}
+
